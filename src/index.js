8ec4586504c40e64a30b66eeeff6f9599eec672d
cesarnavarro1011 <cesarnavarro1011+github@gmail.com>
2022-11-11 21:41:46 -0500

    responsive css card

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import MovieAC from './movieAC+';
-import Session from './component/Session.jsx';
+import MovieAC from './movieAC+';
+// import Session from './component/Session.jsx';
 
 const Root = ReactDOM.createRoot(document.getElementById('root'));
 
 Root.render(
   <React.StrictMode>
-  {/* <MovieAC/> */}
-  <Session/>
+  <MovieAC/>
+  {/* <Session/> */}
   </React.StrictMode>
 );
