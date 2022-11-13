13652caa66ea771f9b87a7ef0fa7671b5d35a4c7
cesarnavarro1011 <cesarnavarro1011+github@gmail.com>
2022-11-13 11:41:39 -0500

    responsive menu

--- src/component/menuPerfil.jsx
+++ src/component/menuPerfil.jsx
@@ -1,23 +1,23 @@
 import styles from "./module-css/menuPerfil.module.css";
 
 export default function CardPerfil() {
-  return (
+    return (
         <ul className={styles.container_menu_perfil}>
-          <li className={styles.container_menu_perfil_list}>
-              <div className={styles.container_menu_container_perfil}>
-                  <span className={styles.container_menu_perfil_circle}>
-                      <img className={styles.img+" "+styles.container_menu_perfil_circle_img} src =""/>
-                  </span>
-                  <div className={styles.container_menu_edit}>
-                      <span className={styles.container_menu_perfil_Name1}>César navarro</span>
-                      <a className={styles.container_menu_perfil_editar_perfil} href ="Perfil.php">Editar Perfil</a>
-                      <p className={styles.container_menu_perfil_type} href ="c">User</p>
-                  </div>
-              </div>
-          </li>
+            <li className={styles.container_menu_perfil_list}>
+                <div className={styles.container_menu_container_perfil}>
+                    <span className={styles.container_menu_perfil_circle}>
+                        <img className={styles.img+" "+styles.container_menu_perfil_circle_img} src =""/>
+                    </span>
+                    <div className={styles.container_menu_edit}>
+                        <span className={styles.container_menu_perfil_Name1}>César navarro</span>
+                        <a className={styles.container_menu_perfil_editar_perfil} href ="Perfil.php">Editar Perfil</a>
+                        <p className={styles.container_menu_perfil_type} href ="c">User</p>
+                    </div>
+                </div>
+            </li>
 
           {/* <li className={styles.container_menu_perfil_list_option}><img  width="20px" height="20px" src=""/><a className={styles.container_menu_perfil_a} href ="Perreo.php">perreo intensivo ;D</a></li> */}
-          <li className={styles.container_menu_perfil_list_option}><object data="f" width="20px" height="20px"></object><a className={styles.container_menu_perfil_a} href ="exit_session.php">Cerrar Session</a></li>
-      </ul>
-  );
+            <li className={styles.container_menu_perfil_list_option}><object data="f" width="20px" height="20px"></object><a className={styles.container_menu_perfil_a} href ="exit_session.php">Cerrar Session</a></li>
+        </ul>
+    );
 }
